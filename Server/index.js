349e24a6f867e5bced28e7a243059e489691b2ef
Eddie <choi.eddie93@gmail.com>
2018-08-24 12:39:07 -0700

    Added local database with methods to handle bin creation and findOne

--- Server/index.js
+++ Server/index.js
@@ -5,6 +5,7 @@ const io = require('socket.io')(http);
 const adminRouter = express.Router();
 const binRouter = express.Router();
 const path = require('path');
+const db = require('./db/')
 
 app.use('/admin', adminRouter);
 
@@ -19,14 +20,28 @@ io.on('connection', function(socket) {
 });
 
 adminRouter.use(express.static('build/admin'));
+
 adminRouter.get('/', (req, res) => {
     res.sendfile(path.resolve(__dirname, 'build/admin/index.html'))
 })
 
-app.post('/')
+binRouter.get(':name', (req, res, next) => {
+    if(req.params.name.split('.')[req.params.name.split('.').length - 1] === 'js' || req.params.name.split('.')[req.params.name.split('.').length - 1] === 'map') {
+        next('route');
+    } 
+    if (db.findOne(res.params.name)) {
+        res.json(db.findOne(res.params.name));
+    } else {
+        res.status(404).json({ error: 'This bin does not exist!!!' });
+    }
+})
 
-app.get('')
+binRouter.use(express.static('build/bin'));
 
+app.post('/', res.status(404))
+
+app.use((req,res) => {
+    res.sendStatus(404)});
 
 http.listen(3000, function() {
     console.log("Listening on Port 3000");
