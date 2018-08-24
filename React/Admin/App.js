8e43a45dd1e1f58b0023121fa99327257f001a59
Jonathan <jonathan.che@outlook.com>
2018-08-24 11:36:33 -0700

    working on changing state from App -> BinCreator

--- React/Admin/App.js
+++ React/Admin/App.js
@@ -4,13 +4,31 @@ import Binlist from './Binlist.js';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      activeBins: [],
+      binName: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.createBin = this.createBin.bind(this);
+
+  }
+
+  createBin() {
+    // do post call here to store bin name in the database. 
+    // get call will be in binList, in the component did mount to display bins in binlist. 
+  }
+
+  handleChange(e) {
+    // update state with the value user has placed in. 
+    this.setState({ binName: e.target.value })
   }
 
   render() {
     return (
       <div>
-        <Bincreator />
+        <Bincreator binName={this.state.binName} handleChange={this.handleChange}/>
         <Binlist />
       </div>
     )
--- React/Admin/Bincreator.js
+++ React/Admin/Bincreator.js
@@ -4,28 +4,12 @@ import Activebins from './Activebins.js';
 class Bincreator extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      binName: '',
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.createBin = this.createBin.bind(this);
-  }
-
-  handleChange(e) {
-    // update state with the value user has placed in. 
-    this.setState({ binName: e.target.value })
-  }
-
-  createBin() {
-    // do post call here to store bin name in the database. 
-    // get call will be in binList, in the component did mount to display bins in binlist. 
   }
 
   render() {
     return (
       <div>
-        <input type="text" onChange={(e) => this.handleChange(e)} value={this.state.binName} />
+        <input type="text" onChange={(e) => this.handleChange(e)} value={this.props.binName} />
         <button onClick={() => this.createBin()}>Create</button>
       </div>
     )
