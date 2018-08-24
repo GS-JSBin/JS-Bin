ae6e170598c6ffc1d10225dadf268473b9c3aed1
Eddie <choi.eddie93@gmail.com>
2018-08-24 15:22:46 -0700

    established routes for creating and deleting new bins

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,9 @@ const adminRouter = express.Router();
 const binRouter = express.Router();
 const path = require('path');
 const db = require('./db/bins.js')
+const bodyparser = require('body-parser');
 
+app.use(bodyparser.json());
 app.use('/admin', adminRouter);
 app.use('/bin', binRouter);
 
@@ -26,15 +28,44 @@ adminRouter.get('/', (req, res) => {
     res.sendfile(path.resolve(__dirname, 'build/admin/index.html'))
 })
 
+// will return an object containing all created bin objects
+adminRouter.get('/allBins', (req,res) => {
+    res.send(db.findAll());
+})
+
+// takes in an object in body containing a key name with the value of the name of the bin to be created
+// and creates an object in the database with the given value
+adminRouter.post('/addBin', (req, res) => {
+    if (!req.body || !req.body.name) return res.status(500).json({ error: 'Must send new bin name.' });
+    db.create(req.body.name);
+    return res.json({ success: "successfully created"});
+})
+
+// takes in an object in body containing a key name with the value of the name of the bin to be deleted
+// deletes the db entry with the given value
+adminRouter.delete('/deleteBin', (req, res) => {
+    if (!req.body || !req.body.name) 
+        return res.status(500).json({ error: 'Must send new bin name.' });
+    if (db.deleteOne(req.body.name)) {
+        return res.json({ success: "sucessfully deleted"})
+    } else {
+        return res.status(500).json({ error: 'bin ' + req.body.name + ' does not exist' })
+    }
+})
+
+
+binRouter.get('/:name/getContent', (req, res) => {
+
+})
+
 binRouter.get('/:name', (req, res, next) => {
     if(req.params.name.split('.')[req.params.name.split('.').length - 1] === 'js' || req.params.name.split('.')[req.params.name.split('.').length - 1] === 'map' || req.params.name.split('.')[req.params.name.split('.').length - 1] === 'css') {
         return next('route');
     } 
     if (db.findOne(req.params.name)) {
         res.sendFile(path.resolve(__dirname, '../build/bin/index.html'));
-        //TODO make route /:name/json that returns json from db res.json(db.findOne(req.params.name));
     } else {
-        res.status(404).json({ error: 'This bin does not exist!!!' });
+        res.sendStatus(404).json({ error: 'This bin does not exist!!!' });
     }
 })
 
