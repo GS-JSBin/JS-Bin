c75286c3145c608637d777fbd3a707db5c32b353
Jonathan <jonathan.che@outlook.com>
2018-08-25 12:03:56 -0700

    working on displaying password in bins

--- React/Admin/Activebins.js
+++ React/Admin/Activebins.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 
 const Activebins = props => {
+  console.log('activebins', props)
+  let displayPassword;
+  if (props.password !== '' || props.password !== undefined) {
+    displayPassword = <button id="displayPassword" disabled="true">{props.password}</button>
+  } 
+
+  console.log('checking display', displayPassword)
   return (
     <div className="bins">
       <span>{props.name}</span>
       <br/>
       <button id={props.name} onClick={(e) => props.deleteBin(e)}>Delete</button>
       <button id={props.name} onClick={(e) => props.redirectToBinPage(e)}>Open</button>
+      {displayPassword}
     </div>
   )
 }
--- React/Admin/Bincreator.js
+++ React/Admin/Bincreator.js
@@ -3,8 +3,15 @@ import React, { Component } from 'react';
 const Bincreator = props => {
   return (
     <div class="wrapper">
+      <label>Bin Name</label>
       <input placeholder="Enter Bin Name Here" type="text" onChange={(e) => props.handleChange(e)} value={props.binName} />
-      <button id="createBin" onClick={(e) => props.createBin(e)}>Create</button>
+      <button id="createBin" onClick={(e) => props.createBin(e)}>Create Bin</button>
+      <br/>
+      <br/>
+      <br/>
+      <label>Password</label>
+      <input type="text" onChange={(e) => props.typePassword(e)} value={props.password} />
+      <button id='filler' disabled='true'></button>
     </div>
   )
 }
